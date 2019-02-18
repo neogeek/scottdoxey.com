b4e4ccb34dd51f46591b65fb2110ee537d4218ee
Scott Doxey <neogeek0101@gmail.com>
2019-02-18 17:16:13 -0500

    Added simple PWA support.

--- index.js
+++ index.js
@@ -207,6 +207,13 @@ process.stdout.write(
                 </p>
             </form>
         </footer>
+        <script>
+            if ('serviceWorker' in navigator) {
+                window.addEventListener('load', () =>
+                    navigator.serviceWorker.register('serviceWorker.js')
+                );
+            }
+        </script>
     </body>
 </html>
 `
