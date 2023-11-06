4fec8673744b7140eafca71632386b1cd2ace3df
Scott Doxey <neogeek0101@gmail.com>
2023-11-06 11:03:37 -0500

    Added mermaid support.

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,9 @@
+const pluginMermaid = require('@kevingimbel/eleventy-plugin-mermaid');
+
 module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy('css');
     eleventyConfig.addPassthroughCopy('images');
     eleventyConfig.addPassthroughCopy('js');
+
+    eleventyConfig.addPlugin(pluginMermaid);
 };
