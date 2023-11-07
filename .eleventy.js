d9d4f5a1ecc8c79fed5ab5ac5dd12958f7cc14b2
Scott Doxey <neogeek0101@gmail.com>
2023-11-07 06:21:07 -0500

    Fixed issue with manifest.json file.

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,11 @@
 const pluginMermaid = require('@kevingimbel/eleventy-plugin-mermaid');
 
 module.exports = function (eleventyConfig) {
-    eleventyConfig.addPassthroughCopy('css');
-    eleventyConfig.addPassthroughCopy('images');
-    eleventyConfig.addPassthroughCopy('js');
+    eleventyConfig.addPassthroughCopy('css/');
+    eleventyConfig.addPassthroughCopy('images/');
+    eleventyConfig.addPassthroughCopy('js/');
+
+    eleventyConfig.addPassthroughCopy('manifest.json');
 
     eleventyConfig.addPlugin(pluginMermaid);
 };
