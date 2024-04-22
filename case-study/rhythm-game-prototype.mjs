29325dbb6c89647b1d9abb0f77c80965a9491e81
Scott Doxey <neogeek0101@gmail.com>
2024-04-22 01:58:49 -0400

    Simplified case study page.

--- case-study/rhythm-game-prototype.mjs
+++ case-study/rhythm-game-prototype.mjs
@@ -6,8 +6,6 @@ import html from '../utilities/html.mjs';
 
 import head from '../includes/head.mjs';
 
-const markdown = await readFile('./rhythm-game-prototype.md', 'utf8');
-
 process.stdout.write(html`<!DOCTYPE html>
   <html lang="en">
     ${head}
@@ -15,7 +13,9 @@ process.stdout.write(html`<!DOCTYPE html>
       <header class="page-header">
         <a href="/">&#8592; Back to Portfolio</a>
       </header>
-      <main class="case-study">${marked.parse(markdown).toString()}</main>
+      <main class="case-study">
+        ${marked.parse(await readFile('./rhythm-game-prototype.md', 'utf8'))}
+      </main>
       <script src="https://cdn.jsdelivr.net/npm/mermaid@10.9.0/dist/mermaid.min.js"></script>
     </body>
   </html>`);
