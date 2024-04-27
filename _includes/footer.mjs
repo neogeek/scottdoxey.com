db4ea1177e56f530fca33e88ac73abd9ee7eb8d5
Scott Doxey <neogeek0101@gmail.com>
2024-04-27 10:45:29 -0400

    Moved social links into one file.

--- _includes/footer.mjs
+++ _includes/footer.mjs
@@ -1,32 +1,7 @@
 import { html } from 'onlybuild';
 
+import socialLinks from './social-links.mjs';
+
 export default html`<footer class="page-footer">
-  <nav class="page-nav">
-    <ul class="social-links">
-      <li>
-        <i class="fa-solid fa-pen-nib"></i>
-        <a href="https://neogeek.dev">Blog</a>
-      </li>
-      <li>
-        <i class="fa-brands fa-github"></i>
-        <a href="https://github.com/neogeek">GitHub</a>
-      </li>
-      <li>
-        <i class="fa-brands fa-linkedin"></i>
-        <a href="https://www.linkedin.com/in/neogeek">LinkedIn</a>
-      </li>
-      <li>
-        <i class="fa-brands fa-speaker-deck"></i>
-        <a href="https://speakerdeck.com/neogeek">Speaker Deck</a>
-      </li>
-      <li>
-        <i class="fa-brands fa-dribbble"></i>
-        <a href="https://dribbble.com/neogeek">Dribbble</a>
-      </li>
-      <li>
-        <i class="fa-brands fa-itch-io"></i>
-        <a href="https://scottdoxey.itch.io/">Itch.io</a>
-      </li>
-    </ul>
-  </nav>
+  <nav class="page-nav">${socialLinks}</nav>
 </footer>`;
--- _includes/header.mjs
+++ _includes/header.mjs
@@ -1,5 +1,7 @@
 import { html } from 'onlybuild';
 
+import socialLinks from './social-links.mjs';
+
 export default html`<header class="page-header">
     <img
       src="/images/avatar.png"
@@ -43,37 +45,4 @@ export default html`<header class="page-header">
     </p>
   </div>
 
-  <nav class="page-nav">
-    <ul class="social-links">
-      <li>
-        <i class="fa-solid fa-pen-nib"></i>
-        <a href="https://neogeek.dev">Blog</a>
-      </li>
-      <li>
-        <i class="fa-brands fa-github"></i>
-        <a href="https://github.com/neogeek">GitHub</a>
-      </li>
-      <li>
-        <i class="fa-brands fa-linkedin"></i>
-        <a href="https://www.linkedin.com/in/neogeek">LinkedIn</a>
-      </li>
-      <li>
-        <i class="fa-brands fa-stack-overflow"></i>
-        <a href="https://stackoverflow.com/users/135018/scott-doxey"
-          >Stack Overflow</a
-        >
-      </li>
-      <li>
-        <i class="fa-brands fa-speaker-deck"></i>
-        <a href="https://speakerdeck.com/neogeek">Speaker Deck</a>
-      </li>
-      <li>
-        <i class="fa-brands fa-dribbble"></i>
-        <a href="https://dribbble.com/neogeek">Dribbble</a>
-      </li>
-      <li>
-        <i class="fa-brands fa-itch-io"></i>
-        <a href="https://scottdoxey.itch.io/">Itch.io</a>
-      </li>
-    </ul>
-  </nav>`;
+  <nav class="page-nav">${socialLinks}</nav>`;
