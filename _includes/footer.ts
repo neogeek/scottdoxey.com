95a12e3050879034e6734860ba80d596193d7fdd
Scott Doxey <neogeek0101@gmail.com>
2024-05-17 15:31:38 -0400

    Updated footer.

--- _includes/footer.ts
+++ _includes/footer.ts
@@ -4,4 +4,7 @@ import socialLinks from './social-links.js';
 
 export default html`<footer class="page-footer">
   <nav class="page-nav">${socialLinks}</nav>
+  <p class="copyright">
+    &copy; Scott Doxey 2006&ndash;${new Date().getFullYear()}
+  </p>
 </footer>`;
