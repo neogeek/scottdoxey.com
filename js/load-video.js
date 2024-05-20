e625719baf473fd9a820885dd9234da0a63744c8
Scott Doxey <neogeek0101@gmail.com>
2024-05-19 21:19:44 -0400

    Prevent showing videos if reduced motion os setting is enabled.

--- js/load-video.js
+++ js/load-video.js
@@ -1,4 +1,12 @@
 (async () => {
+  const prefersReducedMotion = window.matchMedia(
+    `(prefers-reduced-motion: reduce)`
+  ).matches;
+
+  if (prefersReducedMotion) {
+    return false;
+  }
+
   await new Promise(resolve => setTimeout(resolve, 100));
 
   Array.from(document.querySelectorAll('video')).forEach(video => {
